e99037ed5ae50497854c28931afc9aec445f93be
GAHAR-F <messyfiker423@gmail.com>
2025-04-29 11:23:47 +0300

    updated commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// for About
+var tablink = document.getElementsByClassName("tab-link");
+var tabtab = document.getElementsByClassName("tabtab");
+
+function opentab(tabname) {
+  for (tab_link of tablink) {
+    tab_link.classList.remove("Skill");
+  }
+  for (tab_tab of tabtab) {
+    tab_tab.classList.remove("skl");
+  }
+  event.currentTarget.classList.add("Skill");
+  document.getElementById(tabname).classList.add("skl");
+}
+
+// for typed
+const selectTyped = document.querySelector(".typed");
+if (selectTyped) {
+  let typed_strings = selectTyped.getAttribute("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
+}
