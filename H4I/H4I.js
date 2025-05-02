69e638a6660fd4a508e881b32ece9dce2d82313f
GAHAR-F <messyfiker423@gmail.com>
2025-05-02 13:25:47 +0300

    revised

--- H4I/H4I.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting the default way
-    alert("Thank you for contacting us, " + document.getElementById("name").value + "!");
-});
